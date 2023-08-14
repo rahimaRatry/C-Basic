2c24983718259420a2b814f88cb10f0bf2aebe4e
Rahima Akter Ratry <115058821+rahimaRatry@users.noreply.github.com>
2023-08-14 22:15:23 +0600

    Add files via upload

---
+++ Ex-3 .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter a=");
+    scanf("%d", &a);
+
+    printf("Enter b=");
+    scanf("%d",&b);
+
+    int sum= a+b;
+    printf("sum is: %d",sum);
+
+    return 0;
+
+}
