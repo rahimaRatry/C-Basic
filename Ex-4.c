ddf832fb86e32d75edc6da3608f07c286d4e7974
Rahima Akter Ratry <115058821+rahimaRatry@users.noreply.github.com>
2023-08-14 22:15:55 +0600

    Add files via upload

---
+++ Ex-4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//area of square
+
+int main()
+{
+    int side;
+    prinf("Enter the value of side=");
+    scanf("%d", &side);
+    printf("area is: %d", side*side);
+
+    return 0;
+
+}
